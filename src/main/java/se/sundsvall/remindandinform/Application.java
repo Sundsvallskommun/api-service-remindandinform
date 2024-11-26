b5f148931732c9f3544ca0c61861a0b4ec30735e
cheezi0747 <linus.sjolinder@b3.se>
2024-11-26 08:07:38 +0100

    Fixed shedlock bean load issue

--- src/main/java/se/sundsvall/remindandinform/Application.java
+++ src/main/java/se/sundsvall/remindandinform/Application.java
@@ -4,16 +4,13 @@ import static org.springframework.boot.SpringApplication.run;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import se.sundsvall.dept44.ServiceApplication;
 
 @ServiceApplication
 @EnableFeignClients
 @EnableScheduling
-@EnableSchedulerLock(defaultLockAtMostFor = "PT2M")
 public class Application {
-	public static void main(String... args) {
+	public static void main(final String... args) {
 		run(Application.class, args);
 	}
 }
--- src/main/java/se/sundsvall/remindandinform/configuration/ShedLockConfiguration.java
+++ src/main/java/se/sundsvall/remindandinform/configuration/ShedLockConfiguration.java
@@ -1,19 +1,19 @@
 package se.sundsvall.remindandinform.configuration;
 
 import javax.sql.DataSource;
-
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import net.javacrumbs.shedlock.core.LockProvider;
-import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
-
 @Configuration
+@EnableSchedulerLock(defaultLockAtMostFor = "PT2M")
 public class ShedLockConfiguration {
 
 	@Bean
-	public LockProvider lockProvider(DataSource dataSource) {
+	public LockProvider lockProvider(final DataSource dataSource) {
 		return new JdbcTemplateLockProvider(
 			JdbcTemplateLockProvider.Configuration.builder()
 				.usingDbTime()
