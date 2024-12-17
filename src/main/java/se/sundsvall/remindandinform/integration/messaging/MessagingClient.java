5e6ebf3af6e649e5e6a37bb69526f3de85497bf8
cheezi0747 <linus.sjolinder@b3.se>
2024-12-17 10:05:49 +0100

    UF-11348: Add Circuitbreakers

--- src/main/java/se/sundsvall/remindandinform/integration/messaging/MessagingClient.java
+++ src/main/java/se/sundsvall/remindandinform/integration/messaging/MessagingClient.java
@@ -5,6 +5,7 @@ import static se.sundsvall.remindandinform.integration.messaging.configuration.M
 
 import generated.se.sundsvall.messaging.MessageRequest;
 import generated.se.sundsvall.messaging.MessageResult;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,14 +13,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import se.sundsvall.remindandinform.integration.messaging.configuration.MessagingConfiguration;
 
 @FeignClient(name = CLIENT_ID, url = "${integration.messaging.url}", configuration = MessagingConfiguration.class)
+@CircuitBreaker(name = CLIENT_ID)
 public interface MessagingClient {
 
 	/**
 	 * Send messages as email or SMS to list of recipients.
 	 *
-	 * @param  municipalityId                       the municipality ID
-	 * @param  messageRequest                       with a list of messages
-	 * @return                                      messageStatusResponse containing status for transaction.
+	 * @param municipalityId the municipality ID
+	 * @param messageRequest with a list of messages
+	 * @return messageStatusResponse containing status for transaction.
 	 * @throws org.zalando.problem.ThrowableProblem when called service responds with error code
 	 */
 	@PostMapping(path = "/{municipalityId}/messages?async=true", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
